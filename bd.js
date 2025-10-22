d6d336453483a94c45afb18e94cbabd7fa036fc0
JoseOvalle5123 <ovallejuan3124@gmail.com>
2025-10-21 18:48:04 -0600

    se corrigio el db.js

--- bd.js
+++ bd.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/novenoA";
+const MONGODB_URI = process.env.MONGO_URI || "mongodb://localhost:27017/novenoA";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGO_URI);
     console.log("MongoDB Connected Successfully!");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error);
