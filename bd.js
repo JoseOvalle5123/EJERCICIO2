1f256fd298f0bba872631aeb9bfdef26365dc8c1
JoseOvalle5123 <ovallejuan3124@gmail.com>
2025-10-30 11:38:16 -0600

    se implemento el review

--- bd.js
+++ bd.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGO_URI || "mongodb://localhost:27017/novenoA";
+const MONGODB_URI = process.env.MONGO_URI || "mongodb+srv://Rocha1:rocha1@cluster0.1jhuu2u.mongodb.net/novenoA";
 
 export const connectDB = async () => {
   try {
