5e0ec0d9196ead72d3a7c833e6eeb1153cb85c1c
JoseOvalle5123 <ovallejuan3124@gmail.com>
2025-11-21 07:48:37 -0600

    se añadio el campo like

--- models/Card.js
+++ models/Card.js
@@ -5,6 +5,7 @@ const cardSchema = new mongoose.Schema(
     name: { type: String, required: true, trim: true },
     link: { type: String, required: true, trim: true },
     description: { type: String, required: true },
+    like: { type: Boolean }
   },
   {
     timestamps: true,
