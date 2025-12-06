77d83185d3fa5c99e833cb3af2a3f0ad3e79fcb9
JoseOvlle5123 <ovallejuan3124@gmail.com>
2025-12-06 14:40:34 -0600

    se agrego el patch al cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "*",          // Permite cualquier dominio
-    methods: "GET,POST,PUT,DELETE",
+    methods: "GET,POST,PUT,DELETE, PATCH, OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
   })
 );
